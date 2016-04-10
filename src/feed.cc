aa67d00e8a56a3eab5de489e7be02b8d8e21a010
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-04-10 12:28:14 +0530

    testing

--- src/feed.cc
+++ src/feed.cc
@@ -16,23 +16,28 @@ feed::feed(string url){
 bool feed::parse(){
 	try{
 
-			title = json.get<Object>("rss").get<Object>("channel").get<String>("title");
-			link =  json.get<Object>("rss").get<Object>("channel").get<String>("link");
-		    desc = json.get<Object>("rss").get<Object>("channel").get<String>("description");
-			date = json.get<Object>("rss").get<Object>("channel").get<String>("pubDate");
-			items = json.get<Object>("rss").get<Object>("channel").get<Array>("item");
-			News.num_item = 0;
-			for (;News.num_item<items.size();News.num_item++){
-   				 item = new Object(items.get<Object>(News.num_item));
-   				 if(item->has<String>("tile")){
-   				 	News.title[News.num_item] = item->get<String>("tile");
-   				 }
-   				 else{
-   				 	News.title[News.num_item] = item->get<String>("title");
-   				 }
-   				 News.img_path[News.num_item] = item->get<Object>("thumbnail").get<String>("@url");
-   				 News.link[News.num_item] = item->get<String>("link");
-			}
+				title = json.get<Object>("rss").get<Object>("channel").get<String>("title");
+				link =  json.get<Object>("rss").get<Object>("channel").get<String>("link");
+	    	desc = json.get<Object>("rss").get<Object>("channel").get<String>("description");
+				date = json.get<Object>("rss").get<Object>("channel").get<String>("pubDate");
+				items = json.get<Object>("rss").get<Object>("channel").get<Array>("item");
+				News.num_item = 0;
+				for (;News.num_item<items.size();News.num_item++){
+
+   				 	item = new Object(items.get<Object>(News.num_item));
+
+					 	if(item->has<String>("tile")){
+   				 			News.title[News.num_item] = item->get<String>("tile");
+   				 	}
+
+					 	else{
+   				 			News.title[News.num_item] = item->get<String>("title");
+   				 	}
+
+					 	News.img_path[News.num_item] = item->get<Object>("thumbnail").get<String>("@url");
+   				 	News.link[News.num_item] = item->get<String>("link");
+
+				}
 		}catch(...){
 			return false;
 		}
@@ -49,7 +54,7 @@ for(i=0;i<News.num_item;i++){
 		image = download(News.title[News.num_item]+".jpg",News.img_path[News.num_item],true);
       if(image!=NULL){
 
-					News.image[News.num_item] = image;
+				News.image[News.num_item] = image;
 
  	    }else{
 				image = copy_file(".backup.jpg");
